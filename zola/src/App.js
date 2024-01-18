ca1edbe38205bee4725b17cd142a6c15909a51df
ThuyTinh <nguyenthuytinh1409@gmail.com>
2024-01-19 01:39:53 +0700

    home p1

--- zola/src/App.js
+++ zola/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import { Home } from './Components/Home/Home'
 import { Login } from './Components/Login/Login'
 
 function App() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Home />
     </div>
   )
 }
