2740ea38421f095b2f5af74bb6030397c44395f9
Heartnet <thuannguyen242135@gmail.com>
2024-02-19 17:22:14 +0700

    add library, connect to database

---
+++ Zola-Server/src/index.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+
+const app = express()
+const port = 3000
+
+// const route = require('./routes')
+const db = require('./config/db')
+
+//connect to db
+db.connect()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(methodOverride('_method'))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Routes init
+// route(app)
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
