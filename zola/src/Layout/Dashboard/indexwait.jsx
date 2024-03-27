d34ead67cfda284a17d3e020df1f774e74b6abc3
DucTai <69153374+taicutm@users.noreply.github.com>
2024-03-28 02:38:52 +0700

    update user trong dashboardwait
    update user trong dashboardwait

--- zola/src/Layout/Dashboard/indexwait.jsx
+++ zola/src/Layout/Dashboard/indexwait.jsx
@@ -2,18 +2,34 @@ import Main from './main'
 import SideBar from './sideBar'
 import SubSideBar from './subSideBar'
 import Wait from '../Dashboard/main/wait'
+import React, { useState, useEffect, useRef } from 'react'
 
 const DashBoardWait = () => {
+  const [user, setUser] = useState('')
+  const didMountRef = useRef(false)
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
+      return
+    }
+    const userinfor = localStorage.getItem('user')
+    setUser(JSON.parse(userinfor))
+  }, [])
+  console.log('user: ' + JSON.stringify(user))
+  // Nếu user chưa có giá trị, không render gì cả
+  if (!user) {
+    return null
+  }
   return (
     <div
       style={{
         width: '100vw',
         height: '100vh',
         display: 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
       }}
     >
-      <SideBar />
+      <SideBar user={user} />
       <SubSideBar />
       <Wait />
       {/* <Main /> */}
