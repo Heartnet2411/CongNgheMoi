8223d0ebfd0b955ad6f09652af0ce57a34f8a536
Heartnet <thuannguyen242135@gmail.com>
2024-06-14 17:02:47 +0700

    fix link

--- Zola-Server/src/config/db/index.js
+++ Zola-Server/src/config/db/index.js
@@ -2,13 +2,10 @@ import mongoose from 'mongoose'
 
 async function connect() {
     try {
-        await mongoose.connect(
-            'mongodb+srv://vanthuan20069161:nvt12346@thuancluster.cdpasuc.mongodb.net/ZolaChatApp?retryWrites=true&w=majority',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }
-        )
+        await mongoose.connect('', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log('Connect to Database successfully!!!')
     } catch (err) {
         console.log('Connect failure!!!')
